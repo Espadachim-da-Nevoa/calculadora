a14b3c88c3f47135a07e1e261f4999d9594b5512
Kirin <chupacabra123@gentefalsa.com>
2024-06-20 19:49:54 -0300

    First Commit

---
+++ Calculadora que faz todas as 4 operações.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	float n1;
+	printf("Digite o primeiro numero do calculo que deseja fazer: \n");
+	scanf("%f", &n1);
+	
+	char op;
+	printf("digite a operacao que deseja realizar no calculo: \n");
+	scanf("%s", &op);
+	
+	float n2;
+	printf("Digite o ultimo numero do calculo que deseja fazer: \n ");
+	scanf("%f", &n2);
+	
+	if(op == '+'){
+		float n3 = n1 + n2;
+		printf("resultado: %.2f ", n3);
+	}
+	
+	else if(op == '-'){
+		float n3 = n1 - n2;
+		printf("resultado: %.2f ", n3);
+	} 
+	
+	else if(op == '*'){
+		float n3 = n1 * n2;
+		printf("resultado: %.2f ", n3);
+	}
+	
+	else if(op == '/'){
+		float n3 = n1 / n2;
+		printf("resultado: %.2f ", n3);
+	}
+	
+	else{
+		printf("digite as informacoes corretamente");
+	}
+	return 0;
+}
+
+
